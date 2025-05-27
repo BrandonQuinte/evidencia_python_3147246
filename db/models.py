be2847ec84d7cd5c9b69f199889edab50e373f7a
BrandonQuinte <brandonquintero028@gmail.com>
2025-05-27 10:59:12 -0500

    Pyshop con tabla nueva

--- db/models.py
+++ db/models.py
@@ -1,8 +1,16 @@
 from .database import Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 #Crear la clase de modelo(Entidad)
 class Categoria(Base):
     __tablename__ = "categorias"
     id = Column(Integer, primary_key=True, autoincrement=True)
-    nombre = Column(String)
+    nombre = Column(String(50))
+    
+class Productos(Base):
+    __tablename__ =  "productos"
+    nombre = Column(String(40))
+    modelo = Column(String(60))
+    precio = Column(Integer)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    categoria_id = Column(Integer, ForeignKey("categorias.id"))
